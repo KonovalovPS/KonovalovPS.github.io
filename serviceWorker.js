c91fcd0cf94705df49626c24a831398475733079
Pavel Konovalov <konovalov.ps@mail.ru>
2019-04-21 11:06:39 +0300

    bug fixed

--- serviceWorker.js
+++ serviceWorker.js
@@ -6,22 +6,24 @@ if ('serviceWorker' in navigator) {
       // регистрация прошла неудачно
       console.log('Registration failed with 2 ' + error);
     }) 
+
+    this.addEventListener('install', function(event) {
+      event.waitUntil(
+        caches.open('v1').then(function(cache) {
+          return cache.addAll([
+            '/index.html'
+          ]);
+        })
+      );
+    });
+  
+    this.addEventListener('fetch', function(event) {
+      event.respondWith(
+        caches.match(event.request)
+      );
+    });
+
   }else {
     console.log('No service-worker on this browser');
   }
 
-  this.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('v1').then(function(cache) {
-        return cache.addAll([
-          '/index.html'
-        ]);
-      })
-    );
-  });
-
-  this.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-    );
-  });
